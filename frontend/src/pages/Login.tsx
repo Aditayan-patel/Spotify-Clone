08c3358e45cfda594d25ef15646368a15c46de95
Aditayan_Patel <aditayan.patel.cs23@ggits.net>
2026-05-22 10:28:16 +0530

    login update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -21,7 +21,7 @@ const Login = () => {
     <div className="flex items-center justify-center h-screen max-h-screen">
       <div className="bg-black text-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-3xl font-semibold text-center mb-8 bg-gradient-to-b from-white to-green-500 bg-clip-text text-transparent">
-          Login To SpotiWave
+          Login To Spotify
         </h2>
         <form className="mt-8" onSubmit={submitHandler}>
           <div className="mb-4">
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -21,7 +21,7 @@ function Register() {
     <div className="flex items-center justify-center h-screen max-h-screen">
       <div className="bg-black text-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-3xl font-semibold text-center mb-8 bg-gradient-to-b from-white to-green-500 bg-clip-text text-transparent">
-          Register To SpotiWave
+          Register To Spotify
         </h2>
         <form className="mt-8" onSubmit={submitHandler}>
           <div className="mb-4">
