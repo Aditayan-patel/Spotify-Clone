b52fa37f41da805f8f3e66ca2f9e78024364b97e
Aditayan_Patel <aditayan.patel.cs23@ggits.net>
2026-05-22 10:16:57 +0530

    fix all bug

--- frontend/src/components/SongCard.tsx
+++ frontend/src/components/SongCard.tsx
@@ -21,11 +21,11 @@ const SongCard: React.FC<SongCardProps> = ({ image, name, desc, id }) => {
 
   return (
     // Outer div = gradient border
-    <div className="min-w-45 p-[1.5px] m-1 rounded-2xl bg-gradient-to-b from-green-900 via-green-900 to-green-400">
+    <div className="min-w-45 p-[1.5px]  m-1 rounded-2xl bg-gradient-to-b from-green-900 via-green-900 to-green-400">
       {/* Inner div = actual card */}
       <div className="p-2 px-3 rounded-[calc(1rem-1.5px)] cursor-pointer bg-[#171616] hover:bg-[#333131] transition-all duration-300 hover:shadow-[0_0_5px_rgba(255,255,255,0.25)]">
         <div className="relative group">
-          <img src={image} alt={name} className="mr-1 w-40 rounded-md" />
+          <img src={image} alt={name} className="mr-1 w-40 h-40 rounded-md" />
 
           <div className="flex gap-2 cursor-pointer">
             <button className="absolute bottom-1 left-2 bg-green-600 text-black p-3 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-300 hover:shadow-[0_0_5px_rgba(34,197,94,0.6)] cursor-pointer" onClick={()=>{
